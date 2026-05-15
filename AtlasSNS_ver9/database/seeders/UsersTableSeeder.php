7a1d1251b34db1766818cd0862538e33c2adba60
並木裕介 <>
2026-05-15 16:36:30 +0900

    SNS data-assign-2

---
+++ AtlasSNS_ver9/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //ダミーレコードを作成
+        User::factory()->count(10)->create();
+    }
+}
