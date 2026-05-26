22608da547949199d29a685d4800ef6fd5fea822
並木裕介 <>
2026-05-26 16:21:11 +0900

    SNS-part5

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,12 +6,24 @@
   <div class="container">
     <p class="subtitle">AtlasSNSへようこそ</p>
 
+    <div class="form-area">
+
+
         {{ Form::label('メールアドレス')}}
         {{ Form::text('email',null,['class' => 'input']) }}
+        
         {{ Form::label('パスワード')}}
         {{ Form::password('password',['class' => 'input']) }}
 
         {{ Form::submit('ログイン',['class' => 'btn btn-danger']) }}
+
+    
+    
+    </div>
+
+
+
+
 
     <p><a href="register">新規ユーザーの方はこちら</a></p>
     
