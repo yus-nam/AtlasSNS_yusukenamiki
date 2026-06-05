3ee6b8c6afc71cdbd5142582c6f7d1e9d0bbc97f
並木裕介 <>
2026-06-05 15:50:19 +0900

    SNS-20260605-part1

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,7 +4,15 @@
     <h2>機能を実装していきましょう。</h2>
 
     <div class="container">
+        投稿内容を入力してください
+        <button class="btn-type"><img src="{{ asset('images/post.png') }}" alt="送信"></button>
+
+        <button class="btn-type"><img src="{{ asset('images/edit.png') }}" alt="送信"></button>
+
+        <button class="btn-type"><img src="{{ asset('images/trash-h.png') }}" alt="送信"></button>
+
         
+   
  
        
     </div>
