4b3b2e6063ce34f2c8d552ef68b4d37f47c6ed55
並木裕介 <>
2026-06-08 18:16:49 +0900

    SNSkadai-20260608-part1

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,5 +1,7 @@
         <div id="head">
-            <h1><a><img src="images/atlas.png" class="logo"></a></h1>
+            <!-- <h1><a href="#"><img src="images/atlas.png" class="logo"></a></h1> -->
+            <h1><a href="{{ url()->current() }}"><img src="images/atlas.png" class="logo"></a></h1>
+
             <div id="">
                 <div id="">
                     <p class="login-user">〇〇さん</p>
@@ -7,7 +9,7 @@
                 <ul>
                     <li><a href="">ホーム</a></li>
                     <li><a href="">プロフィール</a></li>
-                    <li><a href="">ログアウト</a></li>
+                    <li><a href="login">ログアウト</a></li>
                 </ul>
             </div>
         </div>
